d68bf20bfc28c96d55babc5852316e678b6bd05f
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-01-26 12:40:11 -0800

    edit function

--- 00-exercise/app.js
+++ 00-exercise/app.js
@@ -3,12 +3,13 @@ const form = document.querySelector('#tweetForm');
 const userName = document.querySelector('#name');
 const tweetWord = document.querySelector('#tweet');
 
-document.getElementById("form-button").onclick = function() {
-  document.getElementById("tweets").innerHTML = document.getElementById("tweet").value;
-}
+// document.getElementById("form-button").onclick = function() {
+//   document.getElementById("tweets").innerHTML = document.getElementById("tweet").value;
+// }
 
 form.addEventListener('submit', function(e){
   e.preventDefault();
-  console.log('name', userName.value);
-  console.log('tweet', tweetWord.value);
+  document.getElementById("tweets").innerHTML = document.getElementById("tweet").value;
+  //   console.log('name', userName.value);
+  //   console.log('tweet', tweetWord.value);
 });
